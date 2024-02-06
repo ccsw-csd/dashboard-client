93b20a921169ee444a48e589c970ddadd58ab834
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-02-06 14:50:15 +0100

    Modificado listado de capabilities

--- src/app/catalog/capabilities/capabilities-list/capabilities-list.component.ts
+++ src/app/catalog/capabilities/capabilities-list/capabilities-list.component.ts
@@ -1,65 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { CapabilitiesService } from '../capabilities.service';
-import { SortEvent } from 'primeng/api';
-import { DialogService } from 'primeng/dynamicdialog';
+import { ConfirmationService, SortEvent } from 'primeng/api';
 import { Capability } from 'src/app/core/interfaces/Capability';
+import { ColumnConfigComponent } from 'src/app/core/views/column-config/column-config.component';
+import {
+  DialogService,
+  DynamicDialogConfig,
+  DynamicDialogRef,
+} from 'primeng/dynamicdialog';
+import { Table } from 'primeng/table';
+import { Dropdown } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-capabilities-list',
   templateUrl: './capabilities-list.component.html',
   styleUrls: ['./capabilities-list.component.scss'],
-  providers: [DialogService],
+  providers: [
+    DialogService,
+    DynamicDialogConfig,
+    DynamicDialogRef,
+    ConfirmationService,
+  ],
 })
 export class CapabilitiesListComponent implements OnInit {
-  capabilities: Capability[];
+  @ViewChild(Table) table: Table;
+  @ViewChildren('filterDropdown') filterDropdowns!: QueryList<Dropdown>;
+
+  isSynchronized: Boolean = false;
+  allowSynchronize: Boolean = false;
+  columnNames: any[];
+  selectedColumnNames: any[];
+  changeCols: boolean = false;
+  states: any[];
   tableWidth: string;
   defaultFilters: any = {};
-  selectedColumnNames: any[];
+  totalCapabilities: number = 4;
+  capabilitiesToExport: Capability[];
+
+  capabilities: Capability[];
   years: number[];
   selectedYear: number;
-  columnNames: any[] = [
-    {
-      header: 'Descripción',
-      composeField: 'descripcion',
-      field: 'descripcion',
-      filterType: 'input',
-    },
-    {
-      header: 'Tipo Interfaz',
-      composeField: 'idTipoInterfaz',
-      field: 'idTipoInterfaz',
-      filterType: 'input',
-    },
-    {
-      header: 'NºRegistros',
-      composeField: 'numRegistros',
-      field: 'numRegistros',
-      filterType: 'input',
-    },
-    {
-      header: 'Título',
-      composeField: 'nombreFichero',
-      field: 'nombreFichero',
-      filterType: 'input',
-    },
-    {
-      header: 'Usuario',
-      composeField: 'usuario',
-      field: 'usuario',
-      filterType: 'input',
-    },
-    {
-      header: 'Fecha',
-      composeField: 'fechaImportacion',
-      field: 'fechaImportacion',
-      filterType: 'input',
-    },
-    { header: 'Versión', composeField: 'id', field: 'id', filterType: 'input' },
-  ];
 
-  constructor(private capabilitiesService: CapabilitiesService) {}
+  constructor(
+    private capabilitiesService: CapabilitiesService,
+    private dialogService: DialogService
+  ) {}
 
   ngOnInit() {
+    this.columnNames = [
+      {
+        header: 'Versión',
+        composeField: 'id',
+        field: 'id',
+        filterType: 'input',
+      },
+      {
+        header: 'Descripción',
+        composeField: 'descripcion',
+        field: 'descripcion',
+        filterType: 'input',
+      },
+      {
+        header: 'Fecha de Importación',
+        composeField: 'fechaImportacion',
+        field: 'fechaImportacion',
+        filterType: 'input',
+      },
+      {
+        header: 'Tipo Interfaz',
+        composeField: 'idTipoInterfaz',
+        field: 'idTipoInterfaz',
+        filterType: 'input',
+      },
+      {
+        header: 'NºRegistros',
+        composeField: 'numRegistros',
+        field: 'numRegistros',
+        filterType: 'input',
+      },
+      {
+        header: 'Usuario',
+        composeField: 'usuario',
+        field: 'usuario',
+        filterType: 'input',
+      },
+      {
+        header: 'Título',
+        composeField: 'nombreFichero',
+        field: 'nombreFichero',
+        filterType: 'input',
+      },      
+    ];
+
+    this.selectedColumnNames = this.loadSelected();
+
     this.capabilitiesService.getRolesAvailableYears().subscribe((years) => {
       console.log(years);
       this.years = years;
@@ -77,8 +117,13 @@ export class CapabilitiesListComponent implements OnInit {
       .subscribe((capabilities) => {
         console.log(capabilities);
         this.capabilities = capabilities;
+        this.totalCapabilities = capabilities.length;
         this.setDefaultFilters();
       });
+  }
+
+  onYearChange() {
+    this.loadData(this.selectedYear);
   }
 
   setDefaultFilters() {
@@ -91,10 +136,92 @@ export class CapabilitiesListComponent implements OnInit {
     });
   }
 
+  loadSelected(): any[] {
+    let selectedColumnNames: any = localStorage.getItem(
+      'capabilitiesListColumns'
+    );
+    if (selectedColumnNames == null) return this.columnNames;
+
+    selectedColumnNames = JSON.parse(selectedColumnNames);
+
+    let columns: any[] = [];
+    selectedColumnNames.forEach((item) => {
+      let filterColumn = this.columnNames.filter(
+        (column) => column.header == item
+      );
+      columns = columns.concat(filterColumn);
+    });
+
+    return columns;
+  }
+
+  onFilter(event) {
+    this.capabilitiesToExport = event.filteredValue;
+    setTimeout(() => {
+      this.totalCapabilities = event.filteredValue.length;
+    }, 0);
+  }
+
+  saveSelected(selectedColumnNames: any[]) {
+    localStorage.setItem(
+      'personListColumns',
+      JSON.stringify(selectedColumnNames.map((e) => e.header))
+    );
+  }
+
+  isColumnVisible(field: string): boolean {
+    return this.selectedColumnNames.some((column) => column.field === field);
+  }
+
+  showConfig() {
+    const ref = this.dialogService.open(ColumnConfigComponent, {
+      width: '50vw',
+      data: {
+        columns: this.columnNames,
+        selected: this.selectedColumnNames,
+      },
+      closable: false,
+      showHeader: true,
+      autoZIndex: true,
+      header: 'Configuracion de la tabla',
+    });
+
+    ref.onClose.subscribe((result: any) => {
+      if (result) {
+        this.selectedColumnNames = result;
+        this.saveSelected(result);
+      }
+    });
+  }
+
+  resizeTable() {
+    if (document.getElementById('p-slideMenu')) {
+      this.tableWidth = 'calc(100vw - 255px)';
+    } else {
+      this.tableWidth = 'calc(100vw - 55px)';
+    }
+  }
+
+  onColReorder(event): void {
+    this.saveSelected(this.columnNames);
+  }
+
+  setFilters(): void {
+    this.defaultFilters.active.value = ['1'];
+  }
+
+  cleanFilters(): void {
+    this.filterDropdowns.forEach((dropdown) => dropdown.clear(null));
+    this.table.reset();
+    this.setFilters();
+    this.table.sortOrder = 1;
+    this.table.sort({ field: 'lastname', order: this.table.sortOrder });
+  }
+
   customSort(event: SortEvent) {
     event.data.sort((data1, data2) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
+      let value1 = this.getData(data1, event.field);
+      let value2 = this.getData(data2, event.field);
       let result = null;
 
       if (value1 == null && value2 != null) result = -1;
@@ -119,26 +246,16 @@ export class CapabilitiesListComponent implements OnInit {
     });
   }
 
-  saveSelected(columnNames: any[]) {
-    localStorage.setItem(
-      'capabilities',
-      JSON.stringify(this.columnNames.map((e) => e.header))
-    );
+  getData(data, att) {
+    let atts = att.split('.');
+    atts.forEach((a) => {
+      if (data[a] != undefined) {
+        data = data[a];
+      } else {
+        return null;
+      }
+    });
+    return data;
   }
-
-  onColReorder(event): void {
-    this.saveSelected(this.columnNames);
-  }
-
-  onYearChange() {
-    this.loadData(this.selectedYear);
-  }
-
-  resizeTable() {
-    if (document.getElementById('p-slideMenu')) {
-      this.tableWidth = 'calc(100vw - 255px)';
-    } else {
-      this.tableWidth = 'calc(100vw - 55px)';
-    }
-  }
+  
 }
---
+++ src/app/core/views/column-config/column-config.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+@Component({
+  selector: 'app-personal-config',
+  templateUrl: './column-config.component.html',
+  styleUrls: ['./column-config.component.scss'],
+})
+export class ColumnConfigComponent implements OnInit {
+  constructor(
+    private ref: DynamicDialogRef,
+    private config: DynamicDialogConfig
+  ) {}
+
+  columns: any[] = [];
+  selectedColumns: any[];
+
+  ngOnInit(): void {
+    this.columns = this.getAviables(
+      this.config.data.columns.slice(),
+      this.config.data.selected.slice()
+    );
+    this.selectedColumns = this.config.data.selected.slice();
+  }
+
+  closeWindow() {
+    this.ref.close();
+  }
+
+  save() {
+    this.ref.close(
+      this.getSelected(this.config.data.columns.slice(), this.selectedColumns)
+    );
+  }
+
+  getAviables(cols, sel) {
+    return cols.filter((e) => sel.indexOf(e) == -1);
+  }
+
+  getSelected(cols, sel) {
+    return cols.filter((e) => sel.indexOf(e) != -1);
+  }
+}
