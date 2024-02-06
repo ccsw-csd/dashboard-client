48c0e95f1b1f2c04f60c2459ab382d07bc5fe6d2
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-02-06 14:59:06 +0100

    Añadido componente column-config

--- src/app/core/views/column-config/column-config.component.ts
+++ src/app/core/views/column-config/column-config.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+
 @Component({
   selector: 'app-personal-config',
   templateUrl: './column-config.component.html',
-  styleUrls: ['./column-config.component.scss'],
+  styleUrls: ['./column-config.component.scss']
 })
 export class ColumnConfigComponent implements OnInit {
+
   constructor(
     private ref: DynamicDialogRef,
     private config: DynamicDialogConfig
-  ) {}
+  ) { }
 
   columns: any[] = [];
   selectedColumns: any[];
 
   ngOnInit(): void {
-    this.columns = this.getAviables(
-      this.config.data.columns.slice(),
-      this.config.data.selected.slice()
-    );
+    this.columns = this.getAviables(this.config.data.columns.slice(), this.config.data.selected.slice());
     this.selectedColumns = this.config.data.selected.slice();
   }
 
@@ -27,16 +26,15 @@ export class ColumnConfigComponent implements OnInit {
   }
 
   save() {
-    this.ref.close(
-      this.getSelected(this.config.data.columns.slice(), this.selectedColumns)
-    );
+    this.ref.close(this.getSelected(this.config.data.columns.slice(), this.selectedColumns));
   }
 
   getAviables(cols, sel) {
-    return cols.filter((e) => sel.indexOf(e) == -1);
+    return cols.filter(e => sel.indexOf(e) == -1);
   }
 
   getSelected(cols, sel) {
-    return cols.filter((e) => sel.indexOf(e) != -1);
+    return cols.filter(e => sel.indexOf(e) != -1);
   }
+
 }
