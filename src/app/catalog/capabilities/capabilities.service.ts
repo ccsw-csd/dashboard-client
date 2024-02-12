68acc81c3bdb5c969a79bbc85535bdd35dbba8f9
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-02-12 17:12:17 +0100

    Añadido servicio getAllRoleImportsVersions a capabilities.service

--- src/app/catalog/capabilities/capabilities.service.ts
+++ src/app/catalog/capabilities/capabilities.service.ts
@@ -4,23 +4,25 @@ import { Observable, of } from 'rxjs';
 import { Capability } from 'src/app/core/interfaces/Capability';
 import { environment } from 'src/environments/environment';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CapabilitiesService {
-
   private baseUrl: string = environment.server;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getRoleImportsVersionsByYear(year: number): Observable<Capability[]> {
-    return this.http.get<Capability[]>(`${this.baseUrl}/roleimports/all/${year}`);
+    return this.http.get<Capability[]>(
+      `${this.baseUrl}/roleimports/all/${year}`
+    );
   }
 
   getRolesAvailableYears(): Observable<number[]> {
     return this.http.get<number[]>(`${this.baseUrl}/roleimports/years`);
   }
 
+  getAllRoleImportsVersions(): Observable<Capability[]> {
+    return this.http.get<Capability[]>(`${this.baseUrl}/roleimports/all`);
+  }
 }
